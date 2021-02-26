b063624478c809238a7b49876823d5e6b91693ef
Anthony Hrvojic <1017633+anthfu@users.noreply.github.com>
2021-02-26 12:46:22 -0500

    Added Dockerfiles. Renamed modules and changed root package.

---
+++ basic-producer/src/test/kotlin/com/anthfu/kafka/basicproducer/BasicProducerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.anthfu.kafka.basicproducer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BasicProducerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
