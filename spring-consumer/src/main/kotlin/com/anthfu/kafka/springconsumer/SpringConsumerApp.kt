a97e345d1b7be592f5b3bd1702e33d4f64fb9bd1
Anthony Hrvojic <1017633+anthfu@users.noreply.github.com>
2021-03-02 15:27:20 -0500

    Moved client classes into App file.

--- spring-consumer/src/main/kotlin/com/anthfu/kafka/springconsumer/SpringConsumerApp.kt
+++ spring-consumer/src/main/kotlin/com/anthfu/kafka/springconsumer/SpringConsumerApp.kt
@@ -1,16 +1,30 @@
 package com.anthfu.kafka.springconsumer
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.boot.runApplication
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.stereotype.Component
 
 @ConstructorBinding
-@ConfigurationProperties("spring-consumer")
-data class SpringConsumerProps(
+@ConfigurationProperties("app")
+data class Config(
     val topic: String
 )
+
+@Component
+class SpringConsumer(val config: Config) {
+    val logger: Logger = LoggerFactory.getLogger(SpringConsumer::class.java)
+
+    @KafkaListener(topics = ["#{springConsumer.config.topic}"])
+    fun receive(message: String) {
+        logger.info("Received: $message")
+    }
+}
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
--- spring-producer/src/main/kotlin/com/anthfu/kafka/springproducer/SpringProducerApp.kt
+++ spring-producer/src/main/kotlin/com/anthfu/kafka/springproducer/SpringProducerApp.kt
@@ -1,17 +1,34 @@
 package com.anthfu.kafka.springproducer
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.boot.runApplication
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.stereotype.Component
 
 @ConstructorBinding
-@ConfigurationProperties("spring-producer")
-data class SpringProducerProps(
+@ConfigurationProperties("app")
+data class Config(
     val topic: String
 )
+
+@Component
+class SpringProducer(
+    private val kafkaTemplate: KafkaTemplate<String, String>,
+    private val config: Config
+) {
+    val logger: Logger = LoggerFactory.getLogger(SpringProducer::class.java)
+
+    fun send(message: String) {
+        logger.info("Sent: $message")
+        kafkaTemplate.send(config.topic, message)
+    }
+}
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
