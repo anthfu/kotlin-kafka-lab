78224d93e9590f6c99c97fbc279920a66a82b12f
Anthony Hrvojic <1017633+anthfu@users.noreply.github.com>
2021-03-05 12:03:46 -0500

    Removed `mvn clean package` call from run.sh.

--- run.sh
+++ run.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# Build artifacts
-mvn clean package
-
 # Create topic on local Kafka cluster
 docker run --net=host --rm confluentinc/cp-kafka:latest kafka-topics --create --topic spring-client-stream --replication-factor 1 --partitions 12 --if-not-exists --zookeeper localhost:2181
 
