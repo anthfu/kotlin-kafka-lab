262571af255b21e755aa69a93b42192838aec575
Anthony Hrvojic <1017633+anthfu@users.noreply.github.com>
2021-03-05 16:51:59 -0500

    Ensure tests do not run producer and consumer applications.

--- spring-consumer/src/test/kotlin/com/anthfu/kafka/springconsumer/SpringConsumerAppTests.kt
+++ spring-consumer/src/test/kotlin/com/anthfu/kafka/springconsumer/SpringConsumerAppTests.kt
@@ -1,11 +1,17 @@
 package com.anthfu.kafka.springconsumer
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit.jupiter.SpringExtension
 
-@SpringBootTest
+@ExtendWith(SpringExtension::class)
+@ContextConfiguration(
+    classes = [SpringConsumerApp::class],
+    initializers = [ConfigDataApplicationContextInitializer::class]
+)
 class SpringConsumerAppTests {
     @Test
-    fun contextLoads() {
-    }
+    fun contextLoads() {}
 }
