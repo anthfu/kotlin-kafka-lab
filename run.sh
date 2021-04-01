faf98cdeb6d9e3b9c7ca2a8a38a54d81fc875a22
Anthony Hrvojic <1017633+anthfu@users.noreply.github.com>
2021-04-01 17:14:11 -0400

    Added consumer concurrency property set to default value for visibility. Removed run script.

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Create topic on local Kafka cluster
-docker run --net=host --rm confluentinc/cp-kafka:latest kafka-topics --create --topic spring-client-stream --replication-factor 1 --partitions 6 --if-not-exists --zookeeper localhost:2181
-
-# Spin up producer and consumers
-docker-compose up --scale spring-consumer=3
