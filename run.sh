bcd833bb963ce786c9040cd53de7afdea92ef228
Anthony Hrvojic <1017633+anthfu@users.noreply.github.com>
2021-04-01 13:46:01 -0400

    Moved application properties to yaml. Added Spring config metadata. Avoid building images in Docker Compose now that Maven handles it.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Create topic on local Kafka cluster
-docker run --net=host --rm confluentinc/cp-kafka:latest kafka-topics --create --topic spring-client-stream --replication-factor 1 --partitions 12 --if-not-exists --zookeeper localhost:2181
+docker run --net=host --rm confluentinc/cp-kafka:latest kafka-topics --create --topic spring-client-stream --replication-factor 1 --partitions 6 --if-not-exists --zookeeper localhost:2181
 
 # Spin up producer and consumers
-docker-compose up --build --scale spring-consumer=3
+docker-compose up --scale spring-consumer=3
