0bd46c5765b8c2796ac7f0e37576b220c3647af7
Anthony Hrvojic <1017633+anthfu@users.noreply.github.com>
2021-04-07 15:26:19 -0400

    Integration test WIP.

--- integration-test/src/test/kotlin/com/anthfu/kafka/spring/SpringKafkaClientIT.kt
+++ integration-test/src/test/kotlin/com/anthfu/kafka/spring/SpringKafkaClientIT.kt
@@ -2,13 +2,17 @@ package com.anthfu.kafka.spring
 
 import org.apache.kafka.clients.admin.AdminClient
 import org.apache.kafka.clients.admin.AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG
+import org.apache.kafka.clients.admin.NewTopic
 import org.junit.jupiter.api.Test
+import org.slf4j.LoggerFactory
 import org.testcontainers.containers.GenericContainer
 import org.testcontainers.containers.KafkaContainer
 import org.testcontainers.containers.Network
+import org.testcontainers.containers.output.Slf4jLogConsumer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 import org.testcontainers.utility.DockerImageName
+import java.time.Duration
 
 @Testcontainers
 class SpringKafkaClientIT {
@@ -16,19 +20,25 @@ class SpringKafkaClientIT {
     private val producerImage = DockerImageName.parse("spring-producer:1.0-SNAPSHOT")
     private val consumerImage = DockerImageName.parse("spring-consumer:1.0-SNAPSHOT")
 
+    private val logger = LoggerFactory.getLogger(javaClass)
     private val network = Network.newNetwork()
 
     @Container
-    val kafka: KafkaContainer = KafkaContainer(kafkaImage)
+    private val kafka: KafkaContainer = KafkaContainer(kafkaImage)
         .withNetwork(network)
         .withNetworkAliases("kafka")
 
     @Test
-    fun integrationTest() {
+    fun integration() {
+        val admin = AdminClient.create(mapOf(BOOTSTRAP_SERVERS_CONFIG to kafka.bootstrapServers))
+        admin.createTopics(listOf(NewTopic("spring-kafka-test", 1, 1)))
+
         val producer = GenericContainer<Nothing>(producerImage).apply {
             withNetwork(network)
             withEnv("APP_TOPIC", "spring-kafka-test")
             withEnv("SPRING_KAFKA_PRODUCER_BOOSTRAP-SERVERS", "kafka:9092")
+            withLogConsumer(Slf4jLogConsumer(logger))
+            withMinimumRunningDuration(Duration.ofMinutes(1))
         }
 
         val consumer = GenericContainer<Nothing>(consumerImage).apply {
@@ -36,16 +46,24 @@ class SpringKafkaClientIT {
             withEnv("APP_TOPIC", "spring-kafka-test")
             withEnv("SPRING_KAFKA_CONSUMER_BOOSTRAP-SERVERS", "kafka:9092")
             withEnv("SPRING_KAFKA_CONSUMER_GROUP-ID", "spring-consumers")
+            withLogConsumer(Slf4jLogConsumer(logger))
+            withMinimumRunningDuration(Duration.ofMinutes(1))
         }
 
-        producer.start()
         consumer.start()
+        producer.start()
 
         assert(kafka.isRunning)
         assert(producer.isRunning)
         assert(consumer.isRunning)
 
-        val admin = AdminClient.create(mapOf(BOOTSTRAP_SERVERS_CONFIG to kafka.bootstrapServers))
-        admin.listTopics()
+        val topics = admin.listTopics().names().get()
+
+        assert("spring-kafka-test" in topics)
+
+        admin.close()
+
+        producer.stop()
+        consumer.stop()
     }
 }
