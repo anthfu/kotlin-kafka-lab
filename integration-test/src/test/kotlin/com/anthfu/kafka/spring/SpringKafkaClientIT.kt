c33d28ca871a8c405f79a0dd17d11aeebf207594
Anthony Hrvojic <1017633+anthfu@users.noreply.github.com>
2021-04-07 11:32:20 -0400

    Integration test module WIP.

--- integration-test/src/test/kotlin/com/anthfu/kafka/spring/SpringKafkaClientIT.kt
+++ integration-test/src/test/kotlin/com/anthfu/kafka/spring/SpringKafkaClientIT.kt
@@ -3,23 +3,40 @@ package com.anthfu.kafka.spring
 import org.junit.jupiter.api.Test
 import org.testcontainers.containers.GenericContainer
 import org.testcontainers.containers.KafkaContainer
+import org.testcontainers.containers.Network
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 import org.testcontainers.utility.DockerImageName
 
 @Testcontainers
 class SpringKafkaClientIT {
-    @Container
-    val kafka = KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:latest"))
+    private val network = Network.newNetwork()
 
     @Container
-    val producer = GenericContainer<Nothing>(DockerImageName.parse("spring-producer:1.0-SNAPSHOT"))
-
-    @Container
-    val consumer = GenericContainer<Nothing>(DockerImageName.parse("spring-consumer:1.0-SNAPSHOT"))
+    val kafka: KafkaContainer = KafkaContainer(
+        DockerImageName.parse("confluentinc/cp-kafka:latest")
+    ).withNetwork(network)
 
     @Test
     fun integrationTest() {
-        // TODO
+        val producer = GenericContainer<Nothing>(
+            DockerImageName.parse("spring-producer:1.0-SNAPSHOT")
+        ).apply {
+            withNetwork(network)
+            withEnv("APP_TOPIC", "spring-kafka-test")
+            withEnv("SPRING_KAFKA_PRODUCER_BOOSTRAP-SERVERS", kafka.bootstrapServers)
+        }
+
+        val consumer = GenericContainer<Nothing>(
+            DockerImageName.parse("spring-consumer:1.0-SNAPSHOT")
+        ).apply {
+            withNetwork(network)
+            withEnv("APP_TOPIC", "spring-kafka-test")
+            withEnv("SPRING_KAFKA_CONSUMER_BOOSTRAP-SERVERS", kafka.bootstrapServers)
+            withEnv("SPRING_KAFKA_CONSUMER_GROUP-ID", "spring-consumers")
+        }
+
+        producer.start()
+        consumer.start()
     }
 }
